dd0df6345c19dd4cc9a698461ecb71c783858782
Connor Bach <cbach120@gmail.com>
2018-08-31 15:11:13 -0400

    finished q1

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,25 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Question1 {
 		
 	public Question1(){}
 	
 	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+		List<Integer> ls = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+		Set<Integer> hs = new HashSet<>(ls);
+		
+		int sum = 0;
+		Iterator<Integer> it = hs.iterator();
+		while (it.hasNext()) {
+			sum += it.next().intValue();
+		}
+		return sum;	
 	}
 }
